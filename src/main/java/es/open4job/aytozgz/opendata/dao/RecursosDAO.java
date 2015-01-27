36addc694b96140408afe986390ebe7620113d64
Galvez85 <jmgalvez85@gmail.com>
2015-01-27 11:24:55 +0100

    dao actualizado

---
+++ src/main/java/es/open4job/aytozgz/opendata/dao/RecursosDAO.java
@@ -0,0 +1,27 @@
+package es.open4job.aytozgz.opendata.dao;
+import java.util.List;
+
+import es.open4job.aytozgz.opendata.vo.RecursosVO;
+
+public class RecursosDAO {
+	
+	
+	// Listado de equipamientos
+	public List<RecursosVO> getListadoEquipamientos() {
+		List<RecursosVO> recursos = null;
+		
+		// TODO
+		return recursos;
+	}
+	
+	
+	// Obtiene los datos de un registro en concreto
+	public RecursosVO getDatosEquipamiento(int idEquipo) {
+		RecursosVO recurso = null;
+	
+		// TODO  
+		return recurso;  
+				
+	}
+
+}
